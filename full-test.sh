68a50b1daf50169778557f48d71d7a0a8fee96e9
madhavee777 <madhavee.96@gmail.com>
2026-01-14 22:23:40 +0000

    Fix rootfs path

--- full-test.sh
+++ full-test.sh
@@ -12,6 +12,12 @@ echo "------------------------------------------------"
 # 1. Enter the directory
 cd buildroot
 
+# FIX: Create a symlink so Buildroot finds the external folder locally
+#      It maps 'buildroot/base_external' -> '../base_external'
+if [ ! -d "base_external" ]; then
+    ln -s ../base_external base_external
+fi
+
 # 2. Configure Buildroot
 echo "Loading configuration..."
 make BR2_EXTERNAL=../base_external aesd_qemu_defconfig
