a393b0f796b88fc815c722ffc4165893d134803d
madhavee777 <madhavee.96@gmail.com>
2026-01-14 22:02:11 +0000

    Fix full-test.sh

--- full-test.sh
+++ full-test.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# Stop the script if any command fails
+# Stop on errors
 set -e
 
 echo "------------------------------------------------"
 echo "  Starting Assignment 4 Full Test (Buildroot)"
 echo "------------------------------------------------"
 
-# 1. Enter the buildroot directory
+# 1. Enter the directory
 cd buildroot
 
-# 2. Build the Project
-#    This commands builds the Toolchain, Kernel, and RootFS.
-#    It will take 30-45 minutes on the server.
-echo "Running make..."
+# 2. Configure Buildroot (The Missing Step)
+#    This tells Buildroot to use your settings from 'base_external'
+#    and creates the .config file.
+echo "Loading configuration..."
+make BR2_EXTERNAL=../base_external aesd_qemu_defconfig
+
+# 3. Build the Project
+echo "Building... (This will take 30-45 minutes)"
 make
 
 echo "------------------------------------------------"
