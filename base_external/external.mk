e12172208f62a05de06f5e5a2ca1ef6197aff71e
madhavee777 <madhavee.96@gmail.com>
2026-01-18 03:38:56 +0000

    FIX finder-test: removed ./ from writer

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,1 @@
-include $(BR2_EXTERNAL_AESD_PATH)/package/aesd-assignments/aesd-assignments.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
--- build.sh
+++ build.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-#Script to build buildroot configuration
-#Author: Siddhant Jajoo
+# Script to build the project
+set -e
 
-source shared.sh
+# 1. Enter the buildroot directory
+cd buildroot
 
-EXTERNAL_REL_BUILDROOT=../base_external
-git submodule init
-git submodule sync
-git submodule update
-
-set -e 
-cd `dirname $0`
-
-if [ ! -e buildroot/.config ]
-then
-	echo "MISSING BUILDROOT CONFIGURATION FILE"
-
-	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
-	then
-		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
-	else
-		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
-		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
-	fi
-else
-	echo "USING EXISTING BUILDROOT CONFIG"
-	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
+# 2. Fix the Overlay Path: Create a symlink so Buildroot can find the overlay
+#    The config expects 'base_external/rootfs_overlay' to exist locally.
+if [ ! -d "base_external" ]; then
+    echo "Creating symlink for base_external..."
+    ln -s ../base_external base_external
 fi
+
+# 3. Configure Buildroot
+#    We use realpath for the external tree to ensure variables are set correctly
+echo "Configuring Buildroot..."
+make BR2_EXTERNAL=$(realpath ../base_external) aesd_qemu_defconfig
+
+# 4. Build the Project
+echo "Building Project..."
+make
+
+
+
